040a70fecac7b635f0818a549e6a989f24590f41
key0058 <thekey0058@163.com>
2019-08-13 01:15:02 +0800

    Update vuex

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import store from './store'
 
 Vue.use(Router)
 
@@ -25,11 +26,12 @@ const router = new Router({
 })
 
 router.beforeEach((to, from, next) => {
-  if (to.path.indexOf("home") > 0) {
+  if (!store.state.isAuthority) {
+    store.commit('authorityAccess', true);
     next("/login");
+  } else {
+    next();
   }
-  next();
 })
-
 
 export default router
